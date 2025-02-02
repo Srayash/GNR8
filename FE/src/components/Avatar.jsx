6e3247e29d6a88e765d2ff445af7090eb350ae56
Srayash <srayashsingh995@gmail.com>
2025-02-02 20:26:51 +0530

    Avatar Bug Fixes

--- FE/src/components/Avatar.jsx
+++ FE/src/components/Avatar.jsx
@@ -1,17 +1,27 @@
-export function Avatar(){
-    const name = localStorage.getItem("name");
-    const greeting =  name.split('@')[0][0].toUpperCase() + name.split('@')[0].slice(1);
-    return <>
-        <div className="flex items-center gap-4 font-Bricolage">
-            <div>
-                <p className="text-theme-gray-secondary ">Hey, {greeting}</p>
-            </div>
-            <div className="bg-theme-purple-primary border border-theme-purple-secondary rounded-full">
-                <div className="flex flex-col justify-center items-center h-8 w-8 text-xl">
-                    {name[0].toUpperCase()}
-                </div>
-            </div>
+export function Avatar() {
+    // Retrieve the name from localStorage
+    const storedName = localStorage.getItem("name");
+  
+    // Initialize default values
+    let greeting = "User";
+    let initial = "U";
+  
+    if (storedName != "undefined") {
+        greeting = storedName;
+        initial = storedName[0];
+    }
+  
+    return (
+      <div className="flex items-center gap-4 font-Bricolage">
+        <div>
+          <p className="text-theme-gray-secondary">Hey, {greeting}</p>
         </div>
-    </>
-
-}
+        <div className="bg-theme-purple-primary border border-theme-purple-secondary rounded-full">
+          <div className="flex flex-col justify-center items-center h-8 w-8 text-xl">
+            {initial}
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
