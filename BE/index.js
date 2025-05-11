8ebb65f5efd9055b32cab04f5b6df341b54d67b0
Srayash <srayashsingh995@gmail.com>
2025-05-11 12:56:39 +0530

    deprecate authMiddleware

--- BE/index.js
+++ BE/index.js
@@ -280,15 +280,23 @@ app.delete('/api/v1/user/signout', (req, res, next) => {
 });
 
 app.get("/api/v1/user_data", (req, res) => {
-    if (!req.isAuthenticated()) {
-        return res.status(401).json({ message: "Not authenticated" });
-    }
+  if (req.isAuthenticated()) {
+    // If the user is authenticated, send back the user data
+    // console.log(req.user.name);
+    // console.log(req.user.email);
+    const token = jwt.sign({
+        userId: req.user._id
+    }, process.env.JWT_SECRET);
+
+    res.setHeader("Authorization", `Bearer ${token}`);
 
     res.json({
-        id: req.user._id,
-        email: req.user.email,
-        name: req.user.name,
+      name: req.user.name, // Assuming the user object has a 'name' property
     });
+  } else {
+    // If the user is not authenticated, send an error message
+    res.status(401).json({ message: "Not authenticated" });
+  }
 });
 
 app.get("/session-test", (req, res) => {
