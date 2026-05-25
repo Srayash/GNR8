fdebc81b6f06855fe55e56c96797dc534bc1d20c
Srayash <srayashsingh995@gmail.com>
2026-05-25 16:16:44 +0530

    Remove legacy BE/ and Model/ services

--- BE/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const predictRouter = require("./predict");
-const deployRouter = require("./deploy");
-
-router.use("/generate", predictRouter);
-router.use("/deploy", deployRouter);
-
-module.exports = router;
